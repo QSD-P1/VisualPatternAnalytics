830d7612e4c877022367640f02709f6198ba1b6d
joepvtl <joepvtl@gmail.com>
2022-12-06 22:52:08 +0100

    DPT-64 integrate poc into main solution

---
+++ src/VPA.Client.VisualStudio.Extension.VSIX/Commands/OpenDesignPatternWindow.cs
@@ -0,0 +1,11 @@
+﻿namespace VPA.Client.VisualStudio.Extension.VSIX
+{
+	[Command(PackageIds.OpenDesignPatternWindow)]
+	internal sealed class OpenDesignPatternWindow : BaseCommand<OpenDesignPatternWindow>
+	{
+		protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+		{
+			await DesignPatternToolWindow.ShowAsync();
+		}
+	}
+}
