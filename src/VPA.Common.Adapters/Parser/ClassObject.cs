c52c336085e45877915e786f62e62f4dbc6edd33
Gerke-v-W <S1161016@student.windesheim.nl>
2022-12-16 22:48:53 +0100

    DPT-75 added objects for generic tree

---
+++ src/VPA.Common.Adapters/Parser/ClassObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VPA.Common.Adapters.Parser
+{
+	public class ClassObject: HashSet<ClassObject>,
+	{
+		internal ClassObject() { }
+
+		public static
+	}
+}
---
+++ src/VPA.Common.Adapters/Parser/MethodObject.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VPA.Common.Adapters.Parser
+{
+	internal class Method
+	{
+	}
+}
