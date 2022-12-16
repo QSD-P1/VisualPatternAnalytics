1938bd770859c113950dece6bed428e23de475eb
Gerke-v-W <S1161016@student.windesheim.nl>
2022-12-16 23:09:47 +0100

    DPT-75 add implement node for projectobject

--- src/VPA.Common.Adapters/Parser/ProjectObject.cs
+++ src/VPA.Common.Adapters/Parser/ProjectObject.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace VPA.Common.Adapters.Parser
 {
-	public class ProjectObject
+	public class ProjectObject: Node
 	{
 		public string? Name { get; set; }
 		public List<Node> nodes;
