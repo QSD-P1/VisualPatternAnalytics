0f7789eeb9636a5314622a5f3909c41e55f7822e
joepvtl <joepvtl@gmail.com>
2022-12-20 19:27:29 +0100

    DPT-80 fieldhelper

---
+++ src/VPA.Usecases/Helpers/FieldHelper.cs
@@ -0,0 +1,16 @@
+﻿using VPA.Domain.Enums;
+using VPA.Domain.Models;
+
+namespace VPA.Usecases.Helpers
+{
+	public static class FieldHelper
+	{
+		public static bool ClassHasPublicStaticFieldWithOwnType(ClassNode node)
+		{
+			return node.ChildNodes
+				.OfType<FieldNode>()
+				.Where(n => n.Modifiers.Contains(ModifiersEnum.Static) && n.AccessModifier == AccessModifierEnum.Public)
+				.Any(f => f.Type == node.Name);
+		}
+	}
+}
