21b205eab7c0b6f2de69ab2ee8a1e0235d152e9c
stijn-windesheim <113720499+stijn-windesheim@users.noreply.github.com>
2022-12-20 19:40:54 +0100

    implemented singleton detector

---
+++ src/VPA.Usecases/Detectors/SingletonDetector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using VPA.Domain.Models;
+using VPA.Usecases.DetectionHelpers;
+using VPA.Usecases.Interfaces;
+using VPA.Domain.Enums;
+namespace VPA.Usecases.Usecases
+{
+	public class SingletonDetector : ISingletonDetector
+	{
+		public SingletonDetector()
+		{
+		}
+
+		public async Task<object> Detect(ProjectNode project)
+		{
+			foreach (var classNode in project.ClassNodes)
+			{
+				AccessModifierHelper.AllTypeOfHasAccessModifier<ConstructorNode>(classNode.ChildNodes, AccessModifierEnum.Private);
+			}
+			return new object();
+		}
+	}
+}
---
+++ src/VPA.Usecases/Interfaces/IDetector.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VPA.Domain.Models;
+
+namespace VPA.Usecases.Interfaces
+{
+	public interface IDetector
+	{
+		public Task<object> Detect(ProjectNode tree);
+	}
+}
