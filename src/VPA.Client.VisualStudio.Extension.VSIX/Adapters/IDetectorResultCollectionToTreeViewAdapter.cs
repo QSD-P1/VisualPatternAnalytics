7837f70b1c1e59547b7beb7a39686dd20b718421
StefanStrampel <s1162820@student.windesheim.nl>
2022-12-28 12:24:09 +0100

    DTP-78 Remove old leftover method explanation

--- src/VPA.Client.VisualStudio.Extension.VSIX/Adapters/IDetectorResultCollectionToTreeViewAdapter.cs
+++ src/VPA.Client.VisualStudio.Extension.VSIX/Adapters/IDetectorResultCollectionToTreeViewAdapter.cs
@@ -5,9 +5,6 @@ namespace VPA.Client.VisualStudio.Extension.VSIX.Adapters
 {
 	public interface IDetectorResultCollectionToTreeViewAdapter
 	{
-		/**
-		 * Adapt the adaptee to something else
-		 */
 		public TreeViewItem Adapt(DetectorResultCollection resultCollection);
 	}
 }
