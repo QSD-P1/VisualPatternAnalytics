cf2d4dba95e4c9eacd4bbf2eb275865e665b31ec
stijn-windesheim <113720499+stijn-windesheim@users.noreply.github.com>
2022-12-30 18:56:21 +0100

    moved same interface check to helper method

---
+++ src/VPA.Usecases/DetectionHelpers/InterfaceHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VPA.Domain.Models;
+
+namespace VPA.Usecases.DetectionHelpers
+{
+	public class InterfaceHelper
+	{
+		public static bool HasSameInterface(ClassNode classNode1, ClassNode classNode2, out InterfaceNode? matchedResult)
+		{
+			// initialize the out param
+			matchedResult = null;
+
+			// Searches for the interface
+			string? matchedInterfaceName = classNode1.Interfaces.FirstOrDefault(x => classNode2.Interfaces.Contains(x));
+
+			// If no interface found; return false
+			if (matchedInterfaceName == null) return false;
+
+			// Set matchedResult and return true
+			matchedResult = new InterfaceNode()
+			{
+				Name = matchedInterfaceName
+			};
+			return true;
+		}
+	}
+}
