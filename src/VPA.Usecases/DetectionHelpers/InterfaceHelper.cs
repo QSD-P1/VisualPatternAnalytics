52ac2a36a68d0e388337a3198d4b3288b507a3e8
stijn-windesheim <113720499+stijn-windesheim@users.noreply.github.com>
2023-01-04 00:35:24 +0100

    added check for first classnode

--- src/VPA.Usecases/DetectionHelpers/InterfaceHelper.cs
+++ src/VPA.Usecases/DetectionHelpers/InterfaceHelper.cs
@@ -12,23 +12,22 @@ namespace VPA.Usecases.DetectionHelpers
 			// initialize the out param
 			matchedResult = null;
 
+			// If one of them has no interfaces implemented; return false
+			if (classNode1.Interfaces == null || classNode2.Interfaces == null) return false;
+
 			// Searches for the interface
-			if (classNode2.Interfaces != null)
+			string? matchedInterfaceName = classNode1.Interfaces.FirstOrDefault(x => classNode2.Interfaces.Contains(x));
+
+			// If no identical interface found; return false
+			if (matchedInterfaceName == null) return false;
+
+			// Set matchedResult and return true
+			matchedResult = new InterfaceNode()
 			{
-				string? matchedInterfaceName = classNode1.Interfaces.FirstOrDefault(x => classNode2.Interfaces.Contains(x));
+				Name = matchedInterfaceName
+			};
 
-				// If no identical interface found; return false
-				if (matchedInterfaceName == null) return false;
-
-				// Set matchedResult and return true
-				matchedResult = new InterfaceNode()
-				{
-					Name = matchedInterfaceName
-				};
-
-				return true;
-			}
-			return false;
+			return true;
 		}
 	}
 }
