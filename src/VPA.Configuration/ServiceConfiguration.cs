b799d3629a421b92c7c98e59a86ce99c7db58b65
Hans <s1156878@student.windesheim.nl>
2023-01-08 11:59:11 +0100

    Made some small changes to the defaultconfig

--- src/VPA.Configuration/ServiceConfiguration.cs
+++ src/VPA.Configuration/ServiceConfiguration.cs
@@ -2,7 +2,14 @@
 {
 	public class ServiceConfiguration
 	{
-		public Type? Type { get; set; }
+		/// <summary>
+		/// The type of the service
+		/// </summary>
+		public Type Type { get; set; } = default!;
+
+		/// <summary>
+		/// Bool indicating if a service is presistent (singleton)
+		/// </summary>
 		public bool IsPresistent { get; set; }
 	}
 }
